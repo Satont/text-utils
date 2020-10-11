0060d2799fa99911147606f3a6c3277d19cb248c
Satont <satontworldwide@gmail.com>
2020-10-11 15:59:07 +0300

    Fix test of includesOneOf

--- tests/includesOneOf.test.ts
+++ tests/includesOneOf.test.ts
@@ -1,9 +1,15 @@
 import { expect } from 'chai';
 import { includesOneOf } from '../src/index';
 
-describe('includesOneOf("this is test message", ["test"]) should equals true', function() {
-  it('withSpaces = false', function() {
+describe('includesOneOf', function() {
+  it('includesOneOf("this is test message", ["test"]) equals true', function() {
     const result = includesOneOf('this is test message', ['test'])
     expect(result).equal(true)
   })
+
+  it('includesOneOf("this is test message", ["hello"]) equals false', function() {
+    const result = includesOneOf('this is test message', ['hello'])
+    expect(result).equal(false)
+  })
 })
+
